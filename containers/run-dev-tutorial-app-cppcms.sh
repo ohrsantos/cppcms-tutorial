596e6d9142e2e764fb1663739af81a5a728ade3e
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-02-24 00:06:27 -0300

    .

--- containers/run-dev-tutorial-app-cppcms.sh
+++ containers/run-dev-tutorial-app-cppcms.sh
@@ -1,1 +1,1 @@
-docker run -d  --rm -u root -v /var/run/docker.sock:/var/run/docker.sock  -v /Users/ohrs/it-projects/cppcms-tutorial/containers/instance:/var/www -p 8080:8080  --name dev-tutorial-app-cppcms ohrsan/dev-tutorial-app-cppcms
+docker run -d  --rm -u root -v /var/run/docker.sock:/var/run/docker.sock  -v /Users/ohrs/it-projects/cppcms-tutorial/:/var/www -p 8080:8080  --name dev-tutorial-app-cppcms ohrsan/dev-tutorial-app-cppcms
