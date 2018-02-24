6ddce7cf84547e919edaf8a44bfd46c447abe93f
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-02-24 17:50:42 -0300

    .

--- containers/build-dev-tutorial-app-cppcms.sh
+++ containers/build-dev-tutorial-app-cppcms.sh
@@ -1,6 +1,6 @@
-docker build -f containers/Dockerfile.dev-tutorial-app-cppcms  -t ohrsan/dev-tutorial-app-cppcms:latest . 
+docker build -f containers/Dockerfile.dev-tutorial-app-cppcms  -t ohrsan/dev-tutorial-app-cppcms . 
 
 exit 0
 docker login -u=ohrsan -p=bomdia01
 
-docker push ohrsan/app-cppcms:latest
+docker push ohrsan/app-cppcms
