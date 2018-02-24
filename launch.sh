8e27af0b894cbc63aaad3667fc9c7da7625c5d26
Orlando Hehl Rebelo dos Santos <orlando.hrsantos@gmail.com>
2018-02-24 17:50:53 -0300

    .

--- launch.sh
+++ launch.sh
@@ -1,7 +1,14 @@
+echo ver 001
 cd app
+#make app > make.log 2>&1
 
-IP=$(ip addr  | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -n1 | awk -F'/' '{print $1}')
-sed -i -e "s/CONTAINER_IP/$IP/g" config.js
+#IP=$(ip addr  | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -n1 | awk -F'/' '{print $1}')
+#sed -i -e "s/CONTAINER_IP/$IP/g" config.js
 
 date
-LD_LIBRARY_PATH=/usr/local/lib/ ./app -c config.js 
+date > date.txt
+read
+while true; do
+    LD_LIBRARY_PATH=/usr/local/lib/ ./app -c config.js 
+    sleep 2
+done
